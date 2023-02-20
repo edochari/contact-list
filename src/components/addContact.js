4f7065ea039db93b3c35edbe491f9825ccbc76e1
edochari <120657291+edochari@users.noreply.github.com>
2023-02-20 11:53:55 +0530

    final commit

--- src/components/addContact.js
+++ src/components/addContact.js
@@ -3,20 +3,24 @@ export const AddContact=(props)=>{
     const [name,setName]=useState('');
     const [email,setEmail]=useState('');
     const [phone,setPhone]=useState(0);
+    
     const handleSubmit=(e)=>{
         e.preventDefault();
+        console.log("name",name);
          props.onAddingContact(name,email,phone);
        
     }
     return (
+        
         <form onSubmit={handleSubmit}>
-            <label>Name</label>
-            <input type="text" onChange={(e)=>setName(e.target.value)}/>
-            <label>email</label>
-            <input type="email" onChange={(e)=>setEmail(e.target.value)}/>
-            <label>Phone</label>
-            <input type="number" onChange={(e)=>setPhone(e.target.value)}/> 
-            <button type="submit">Submit</button>
+            <label className="input-field">Name</label>
+            <input type="text" className="input-field" onChange={(e)=>setName(e.target.value)}/>
+            <label className="input-field">email</label>
+            <input type="email" className="input-field" onChange={(e)=>setEmail(e.target.value)}/>
+            <label className="input-field">Phone</label>
+            <input type="number" className="input-field" onChange={(e)=>setPhone(e.target.value)}/>
+            
+            <button type="submit" className="input-field" >Submit</button>
         </form>
     )
 }
